de7c49f50322267d63058ab7b457b35ccf4b81cd
angelo-zamora <angelo.zamora@sanyutechnos-ph.com>
2026-07-23 10:45:53 +0800

    add notification for overtime users still clock in for 3 hours

--- src/functions/OvertimeClockOutReminder/index.js
+++ src/functions/OvertimeClockOutReminder/index.js
@@ -1,7 +1,7 @@
 const { app } = require("@azure/functions");
 const notificationService = require("../../services/NotificationService");
 const logger = require("../../helpers/Logger");
-const constant = require("../../helpers/Constants");
+const constant = require("../../helpers/Constants");c
 
 app.timer("OvertimeClockOutReminder", {
     schedule: constant.OVERTIME_REMINDER,
